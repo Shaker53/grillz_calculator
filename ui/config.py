123cca4f9265b0dfd6751680ead3975b4ab632c3
ilya <ilya.pashkevich>
2020-05-06 13:30:41 +0300

    built practically all the UI

--- ui/config.py
+++ ui/config.py
@@ -1,4 +1,5 @@
-from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QCoreApplication, QSize, QRect
+from PyQt5.QtGui import QFont, QColor
 
 
 class Font(QFont):
@@ -14,3 +15,93 @@ class Font(QFont):
 bigfont = Font(14)
 mainfont = Font()
 littlefont = Font(11)
+
+window_title = QCoreApplication.translate('window', 'GRILLZ TURBOCALCULATOR')
+
+table_rows = 10
+table_columns = 5
+
+items_text = {
+    'label_n_of_order': ' Номер заказа* ',
+    'label_client_name': ' Имя заказчика*  ',
+    'label_start_date': 'Дата заказа',
+    'label_maybe_finish_date': 'Дата окончания ',
+    'label_order_sum': ' Общ. сумма заказа* ',
+    'label_first_payment': ' Первый взнос  ',
+    'label_jaws': 'Челюсти  ',
+    'label_n_of_teeth': ' Количество  зубов* ',
+    'label_spraying': 'Напыление ',
+    'label_add_costs': 'Доп.расходы',
+    'check_difficult': 'Cложная работа,\nдополнительные\nукрашения и т.д.',
+    'input_jaws': [3, 'нижняя', 'верхняя', 'нижн. и верх.'],
+    'button_calculate': 'Рассчитать',
+    'button_return': 'Назад',
+    'button_save': 'Сохранить заказ',
+    'msg_exelsaved_window_title': 'Пора за работу',
+    'msg_exelsaved_text': '   Заказ успешно сохранен!   ',
+    'msg_not_all_important_params_window_title': 'Внимание!',
+    'msg_not_all_important_params_text': 'Вы заполнили не все обязательные поля\n',
+    'msg_not_all_important_params_button_ok': '  Сейчас заполню  ',
+    'msg_not_all_important_params_button_no': '  Плевать  ',
+    'msg_wrong_input_window_title': 'Внимание!',
+    'msg_wrong_input_text': 'Вы неправильно заполнили поля ввода\n',
+
+
+}
+
+items_coordinates = {
+    'label_n_of_order': [0, 1, 1, 1],
+    'label_client_name': [2, 1, 1, 1],
+    'label_start_date': [4, 1, 1, 1],
+    'label_maybe_finish_date': [6, 1, 1, 1],
+    'label_order_sum': [0, 3, 1, 1],
+    'label_first_payment': [2, 3, 1, 1],
+    'label_jaws': [0, 5, 1, 1],
+    'label_n_of_teeth': [2, 5, 1, 1],
+    'label_spraying': [4, 5, 1, 1],
+    'label_add_costs': [6, 5, 1, 1],
+
+    'input_n_of_order': [1, 1, 1, 1],
+    'input_client_name': [3, 1, 1, 1],
+    'input_order_sum': [1, 3, 1, 1],
+    'input_first_payment': [3, 3, 1, 1],
+    'input_difficult_sum': [7, 3, 1, 1],
+    'input_n_of_teeth': [3, 5, 1, 1],
+    'input_spraying': [5, 5, 1, 1],
+    'input_add_costs': [7, 5, 1, 1],
+
+    'spacer1': [4, 2, 1, 1],
+    'spacer2': [4, 0, 1, 1],
+    'spacer3': [8, 3, 1, 1],
+    'spacer4': [4, 6, 1, 1],
+    'spacer5': [4, 4, 1, 1],
+
+    'input_maybe_finish_date': [7, 1, 1, 1],
+    'input_start_date': [5, 1, 1, 1],
+
+    'check_difficult': [4, 3, 3, 1],
+    'input_jaws': [1, 5, 1, 1],
+    'button_calculate': [9, 3, 1, 1]
+}
+
+items_geometry = {
+    'window': QSize(824, 478),
+    'stackedWidget': QRect(0, -16, 811, 480),
+    'layoutWidget': QRect(0, 20, 825, 426),
+    'page_2': QSize(811, 480),
+    'tableWidget': QRect(5, 25, 804, 394),
+
+    'table_items_height': 39,
+    'table_items_width': 160,
+
+    'button_return': QRect(180, 434, 203, 40),
+    'button_save': QRect(430, 434, 203, 40),
+    'button_calculate': QRect(430, 434, 218, 40),
+    'spacers': [40, 20]
+}
+
+colors = {
+    'centralWidget': 'color: rgb(31, 85, 94)',
+    'table_separators': QColor(193, 193, 193),
+    'table_main_items': QColor(205, 245, 255)
+}
--- ui/forms.py
+++ ui/forms.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtGui import QIcon
 from ui import config
 
 
@@ -54,3 +55,132 @@ class Button(QtWidgets.QPushButton):
         super().__init__(config.items_text[name], placement)
         self.setGeometry(config.items_geometry[name])
         self.setFont(config.mainfont)
+
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui(self)
+
+    def setup_ui(self, window):
+        build_window(self, window)
+        centralize(self)
+        self.setWindowIcon(QIcon('icons/bombucha.png'))
+        build_page_with_inputs(self)
+        build_page_with_table(self)
+        set_button_actions(self)
+
+    def return_clicked(self):
+        self.stackedWidget.setCurrentIndex(0)
+        self.button_save.setEnabled(True)
+
+    def enable_difficult(self):
+        if not self.difficult_sum.isEnabled():
+            self.difficult_sum.setEnabled(True)
+            self.difficult_sum.setFocus()
+        else:
+            self.difficult_sum.clear()
+            self.difficult_sum.setEnabled(False)
+
+
+def set_button_actions(ui):
+    ui.check_difficult.clicked.connect(ui.enable_difficult)
+    ui.button_return.clicked.connect(ui.return_clicked)
+
+
+def centralize(ui):
+    placement = ui.frameGeometry()
+    screen_center = QtWidgets.QDesktopWidget().availableGeometry().center()
+    placement.moveCenter(screen_center)
+    ui.move(placement.topLeft())
+
+
+def build_window(ui, window):
+    ui.window = window
+    ui.window.setWindowTitle(config.window_title)
+    ui.window.setMinimumSize(config.items_geometry['window'])
+    ui.window.setMaximumSize(config.items_geometry['window'])
+    build_centralWidget(ui)
+
+
+def build_centralWidget(ui):
+    ui.centralWidget = QtWidgets.QWidget(ui.window)
+    ui.centralWidget.setMinimumSize(config.items_geometry['window'])
+    ui.centralWidget.setMaximumSize(config.items_geometry['window'])
+    ui.centralWidget.setStyleSheet(config.colors['centralWidget'])
+    ui.window.setCentralWidget(ui.centralWidget)
+    build_stackedWidget(ui)
+
+
+def build_stackedWidget(ui):
+    ui.stackedWidget = QtWidgets.QStackedWidget(ui.centralWidget)
+    ui.stackedWidget.setGeometry(config.items_geometry['stackedWidget'])
+
+
+def build_page_with_inputs(ui):
+    ui.page1 = QtWidgets.QWidget()
+    ui.stackedWidget.addWidget(ui.page1)
+    ui.layoutWidget = QtWidgets.QWidget(ui.page1)
+    ui.layoutWidget.setGeometry(config.items_geometry['layoutWidget'])
+    ui.gridLayout = QtWidgets.QGridLayout(ui.layoutWidget)
+
+    ui.spacer1 = Spacer('spacer1', ui.gridLayout)
+    ui.spacer2 = Spacer('spacer2', ui.gridLayout)
+    ui.spacer3 = Spacer('spacer3', ui.gridLayout)
+    ui.spacer4 = Spacer('spacer4', ui.gridLayout)
+    ui.spacer5 = Spacer('spacer5', ui.gridLayout)
+
+    ui.button_calculate = Button('button_calculate', placement=ui.page1)
+    ui.gridLayout.addWidget(ui.button_calculate, *config.items_coordinates['button_calculate'])
+
+    build_all_input_fields(ui)
+
+
+def build_all_input_fields(ui):
+    ui.label_n_of_order = Label('label_n_of_order', ui.gridLayout)
+    ui.label_client_name = Label('label_client_name', ui.gridLayout)
+    ui.label_start_date = Label('label_start_date', ui.gridLayout)
+    ui.label_maybe_finish_date = Label('label_maybe_finish_date', ui.gridLayout)
+    ui.label_order_sum = Label('label_order_sum', ui.gridLayout)
+    ui.label_first_payment = Label('label_first_payment', ui.gridLayout)
+    ui.label_jaws = Label('label_jaws', ui.gridLayout)
+    ui.label_n_of_teeth = Label('label_n_of_teeth', ui.gridLayout)
+    ui.label_spraying = Label('label_spraying', ui.gridLayout)
+    ui.label_add_costs = Label('label_add_costs', ui.gridLayout)
+
+    ui.n_of_order = LineInput('input_n_of_order', ui.gridLayout)
+    ui.client_name = LineInput('input_client_name', ui.gridLayout)
+    ui.order_sum = LineInput('input_order_sum', ui.gridLayout)
+    ui.first_payment = LineInput('input_first_payment', ui.gridLayout)
+    ui.difficult_sum = LineInput('input_difficult_sum', ui.gridLayout)
+    ui.n_of_teeth = LineInput('input_n_of_teeth', ui.gridLayout)
+    ui.spraying = LineInput('input_spraying', ui.gridLayout)
+    ui.add_costs = LineInput('input_add_costs', ui.gridLayout)
+    ui.difficult_sum.setEnabled(False)
+
+    ui.maybe_finish_date = DateInput('input_maybe_finish_date', ui.gridLayout)
+    ui.start_date = DateInput('input_start_date', ui.gridLayout)
+    ui.check_difficult = CheckBox('check_difficult', ui.gridLayout)
+    ui.jaws = ComboBox('input_jaws', ui.gridLayout)
+
+
+def build_page_with_table(ui):
+    ui.page2 = QtWidgets.QWidget()
+    ui.page2.setMinimumSize(config.items_geometry['page_2'])
+    ui.stackedWidget.addWidget(ui.page2)
+
+    ui.button_return = Button('button_return', placement=ui.page2)
+    ui.button_save = Button('button_save', placement=ui.page2)
+    build_table_frame(ui)
+
+
+def build_table_frame(ui):
+    ui.tableWidget = QtWidgets.QTableWidget(ui.page2)
+    ui.tableWidget.setGeometry(config.items_geometry['tableWidget'])
+    ui.tableWidget.setRowCount(config.table_rows)
+    ui.tableWidget.setColumnCount(config.table_columns)
+
+    ui.tableWidget.horizontalHeader().setVisible(False)
+    ui.tableWidget.horizontalHeader().setDefaultSectionSize(config.items_geometry['table_items_width'])
+    ui.tableWidget.verticalHeader().setVisible(False)
+    ui.tableWidget.verticalHeader().setDefaultSectionSize(config.items_geometry['table_items_height'])
